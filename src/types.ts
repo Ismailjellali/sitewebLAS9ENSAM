ce68c787b4c18781bfdc151802dac90969fae2de
Ismail jellali <ismail_jellali@um5.ac.ma>
2025-02-01 17:12:48 +0100

    types.ts

--- src/types.ts
+++ src/types.ts
@@ -26,56 +26,60 @@ export const filieres: Filiere[] = [
   { id: 'api', name: 'API', color: 'teal', semesters: 4 },
 ];
 
-// Sample modules for GEIN (Add more for other filières)
 export const modules: Module[] = [
   // GEIN Semester 1
   {
-    id: 'gein-s1-1',
-    name: 'Programmation Orientée Objet Java',
-    code: 'GEIN101',
+    id: 'gein-s1-m111',
+    name: 'Systèmes automatisés',
+    code: 'M111',
     filiereId: 'gein',
     semester: 1,
-    youtubeUrl: 'https://youtube.com/playlist?list=your-playlist-id',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id'
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbFMafWwNUF9vE0Q1lSUYax1',
+    driveUrl: 'https://drive.google.com/drive/folders/1J3zRlaKEnc5GqfdDsLjp6NUE_yGiRvwW?usp=drive_link'
   },
   {
-    id: 'gein-s1-2',
-    name: 'Structures de Données Avancées',
-    code: 'GEIN102',
+    id: 'gein-s1-m112',
+    name: 'Electronique analogique',
+    code: 'M112',
     filiereId: 'gein',
     semester: 1,
-    youtubeUrl: 'https://youtube.com/playlist?list=your-playlist-id',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id'
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbGBf5t63FieACDdsCzeRFiC',
+    driveUrl: 'https://drive.google.com/drive/folders/1hQ5RIt1EeNrMx95Ve0uRY0K97vWU06xi?usp=drive_link'
   },
   {
-    id: 'gein-s1-3',
-    name: 'Architecture des Ordinateurs',
-    code: 'GEIN103',
+    id: 'gein-s1-m113',
+    name: 'Technologies des systèmes mécaniques',
+    code: 'M113',
     filiereId: 'gein',
     semester: 1,
-    youtubeUrl: 'https://youtube.com/playlist?list=your-playlist-id',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id'
-  },
-  
-  // GEIN Semester 2
-  {
-    id: 'gein-s2-1',
-    name: 'Bases de Données Avancées',
-    code: 'GEIN201',
-    filiereId: 'gein',
-    semester: 2,
-    youtubeUrl: 'https://youtube.com/playlist?list=your-playlist-id',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id'
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbFLPsJl_K0BT5ArP8ir4Z1c',
+    driveUrl: 'https://drive.google.com/drive/folders/12o8WobY8uY2m8Fm6gga3VsiQlgs35opp?usp=drive_link'
   },
   {
-    id: 'gein-s2-2',
-    name: 'Développement Web',
-    code: 'GEIN202',
+    id: 'gein-s1-m114',
+    name: 'Bases de l\'électrotechnique et de l\'électronique de puissance',
+    code: 'M114',
     filiereId: 'gein',
-    semester: 2,
-    youtubeUrl: 'https://youtube.com/playlist?list=your-playlist-id',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id'
+    semester: 1,
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbHTvvN8Fu1WW0zA9yXnq0HD',
+    driveUrl: 'https://drive.google.com/drive/folders/1FRA1Y_Hpz0-ec5TMhbDlriPQSUp_n8RE?usp=drive_link'
   },
-  
-  // Add more modules for other semesters and filières...
+  {
+    id: 'gein-s1-m115',
+    name: 'Mathématiques pour l\'ingénieur',
+    code: 'M115',
+    filiereId: 'gein',
+    semester: 1,
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbGG7ezNaoVOrnXH6GyWhXON',
+    driveUrl: 'https://drive.google.com/drive/folders/1t0T7Io3XPV5CCnrIjQw-xMXDdFYjG2XN?usp=drive_link'
+  },
+  {
+    id: 'gein-s1-m117',
+    name: 'Compétences digitales et informatiques',
+    code: 'M117',
+    filiereId: 'gein',
+    semester: 1,
+    youtubeUrl: 'https://www.youtube.com/playlist?list=PL8K5Y6-aFNbG9Yz-b833jWgVn44uLjoyw',
+    driveUrl: 'https://drive.google.com/drive/folders/1vR2TRlO35j_DM-CiU7kdF--jQJSJE5dj?usp=drive_link'
+  }
 ];
